a089e7f1fac3fb13a1226e0ad01c55981903c099
Jubayed Hossain <70032786+hmjubayed@users.noreply.github.com>
2020-11-30 14:53:06 +0600

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -21,28 +21,4 @@ if (! function_exists('notify')) {
     }
 }
 
-if (! function_exists('notify_js')) {
-    /**
-     * @return string
-     */
-    function notify_js(): string
-    {
-        $driver  = config('notify.default');
-        $scripts = config('notify.'.$driver.'.notify_js');
 
-        return '<script type="text/javascript" src="'.implode('"></script><script type="text/javascript" src="', $scripts).'"></script>';
-    }
-}
-
-if (! function_exists('notify_css')) {
-    /**
-     * @return string
-     */
-    function notify_css(): string
-    {
-        $driver  = config('notify.default');
-        $styles = config('notify.'.$driver.'.notify_css');
-
-        return '<link rel="stylesheet" type="text/css" href="'.implode('"><link rel="stylesheet" type="text/css" href="', $styles).'">';
-    }
-}
